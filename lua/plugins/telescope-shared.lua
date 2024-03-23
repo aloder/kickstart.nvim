4147e7128d2e2e2a6c3ff7133fd7e35c28b07e91
Andrew Loder <aloder@google.com>
2024-03-23 19:37:12 +0000

    boo

--- lua/plugins/telescope-shared.lua
+++ lua/plugins/telescope-shared.lua
@@ -31,7 +31,6 @@ function M.Shared_keybindings()
   vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
   vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
   vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-  vim.keymap.set('n', '<leader>so', require("telescope").extensions.aerial.aerial, { desc = '[S]earch by [O]utline' })
 end
 
 return M
